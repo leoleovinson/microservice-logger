163fc29ffe42f6605898e2ecbbebfc2c8b93f865
leovinson <95406073+leovinson@users.noreply.github.com>
2022-03-04 14:17:30 +0800

    -logger microservice

---
+++ src/main/java/com/leo/microservice/loggerservice/service/LogService.java
@@ -0,0 +1,51 @@
+package com.leo.microservice.loggerservice.service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.leo.microservice.loggerservice.dto.MessageLogDto;
+import com.leo.microservice.loggerservice.entity.MessageLog;
+import com.leo.microservice.loggerservice.repository.MessageLogRepository;
+import com.leo.microservice.loggerservice.utilities.LogConverter;
+
+@Service
+public class LogService {
+	
+	@Autowired
+	LogConverter logConverter;
+	
+	@Autowired
+	MessageLogRepository logRepository;
+	
+	@KafkaListener(groupId = "logs-topic-group-1", topics = "logs-topic")
+	public void getMessageFromTopic(String data) {
+		System.out.println("ARE YOU LISTENING" +data);
+		logToDatabase(data);
+//		System.out.println(data);
+	}
+	
+	public void logToDatabase(String data) {
+		MessageLogDto messageLogDto = new MessageLogDto();
+		messageLogDto.setDateCreated(new Date());
+		messageLogDto.setMessage(data);
+//		System.out.println("asdasd "+messageLogDto);
+//		MessageLog messageLog = new MessageLog();
+//		messageLog.setMessage(messageLogDto.getMessage());
+//		System.out.println("2nd asdasd " +messageLog);
+		logRepository.save(logConverter.dtoToEntity(messageLogDto));
+	}
+	
+	public List<MessageLog> getAllLogs(){
+		return logRepository.findAll();
+	}
+	
+	public List<MessageLogDto> getAllLogsDto(){
+		return logRepository.findAll().stream().map(logs->logConverter.entityToDto(logs)).toList();
+	}
+
+}
